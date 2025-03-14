f11a4ce0a1ac409d5ebf6275a03aa5af2d40add5
YeilySuan <yeilyortiz25@gmail.com>
2025-03-13 21:06:16 -0500

    2025-03-13-AAAA

--- OpenTerminalYAddCommitPushGit.bat
+++ OpenTerminalYAddCommitPushGit.bat
@@ -5,11 +5,15 @@ cd /d "%~dp0"
 for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
 set today=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
 
+:: Obtener la hora actual en formato HHMM
+set hour=%time:~0,2%
+if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
+
 :: Generar 4 letras aleatorias usando una semilla basada en la hora actual
 setlocal enabledelayedexpansion
 set "letters=ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 set "randomStr="
-set /a "seed=!random! * !time:~-3,1!"
+set /a "seed=!random! * !hour!"
 
 for /L %%i in (1,1,4) do (
     set /a "rand=!seed! * %%i %% 26"
@@ -25,5 +29,6 @@ git commit -m "%commitMessage%"
 git push origin main
 
 endlocal
+
 
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,7 @@
   async function logginSuccess(logginCedula, logginPassword) {
   try {
     // Esto es lo que cambie para leer en Railway DESPLIEGUE
-    const response = await fetch('https://sirem-project-production-e2cc.up.railway.app/api/validateLogginToMainMenu', {
+    const response = await fetch('/api/validateLogginToMainMenu', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
